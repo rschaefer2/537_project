cf2fe69272dbea6e0c1fc9c583383bf65d813fa9
Brandon <bschaef14@gmail.com>
2015-10-20 12:50:13 -0500

    No longer serializing, sending first 5 bytes as frame number and next 1024 bytes is data. Req from client is fixed 40 bytes with first 5 as frame number.
    Changed receive method to attempt to receive only 40 bytes of data, need to test this.

--- UDPServer.py
+++ UDPServer.py
@@ -2,17 +2,9 @@ __author__ = 'bvschaefer'
 
 import socket
 import sys
-import random
+import ast
 import select
 import pickle
-
-class FrameReq:
-    frame_number = 0
-    movie_title = ""
-
-    def __init__(self, frame_number, movie_title):
-        self.frame_number = frame_number
-        self.movie_title = movie_title
 
 class FrameResp:
     frame_number = 0
@@ -33,7 +25,7 @@ sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 sock.bind(server_address)
 
 # create an empty dictionary to save received data
-# data_dict = {}
+data_dict = {}
 # socket_list must be a sequence
 socket_list = [sock]
 
@@ -43,26 +35,37 @@ while True:
 
     for s in read_sockets:
         if s == sock:
-            data, address = s.recvfrom(FrameReq.__sizeof__())
+            recv_size = 40
+            data = 0
+            data, address = s.recvfrom(recv_size)
+            recv_size -= len(data)
+            while (recv_size != 0):
+                temp_data, address = s.recvfrom(recv_size)
+                recv_size -= len(data)
+                data += temp_data
+
             # check if data is valid
             if not data:
                     sys.stdout.write('\nDisconnected from server')
             else :
                 # deserialize request from client
-                frame_request = pickle.load(data)
+                # frame_request = pickle.load(data)
 
                 # print data for now
-                sys.stdout.write(frame_request.movie_title)
-                sys.stdout.write(frame_request.frame_number)
+                # sys.stdout.write(frame_request.movie_title)
+                # sys.stdout.write(frame_request.frame_number)
 
-                with open(frame_request.movie_title) as fin:
-                    fin.seek(frame_request.frame_number * 1024)
-                    data = fin.read(1024)
-                    send_frame = FrameResp(frame_request.frame_number, frame_request.movie_title, data)
+                # save data received in string
+                # send response if valid data
+                        with open(data[5:]) as fin:
+                            fin.seek(int(data[:5]) * 1024)
+                            send_data = fin.read(1024)
+                            barray = bytearray()
+                            barray.extend(data[:5])
+                            barray.extend(send_data)
 
-                    # send response
-                    s.sendto(pickle.dumps(send_frame), address)
-    break
+                            # send response
+                            s.sendto(barray, address)
 
 
 
