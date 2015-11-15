25b116779ef4b425dc779571e6e8d9b21175f325
Tommi Brandstack <tommi.brandstack@gmail.com>
2015-11-15 15:11:10 -0600

    Fixed errors

--- Qos.py
+++ Qos.py
@@ -1,15 +1,18 @@
 import time
+import socket
+import sys
 
-class Gos:
-    def __init__(self, lock, main_list, active_list):
+
+class QoS:
+    def __init__(self, lock, global_server_list, active_server_list):
         self.lock = lock
-        self.main_list = main_list
-        self.active_list = active_list
+        self.global_server_list = global_server_list
+        self.active_server_list = active_server_list
         self.THRESHOLD = 10
 
     def start(self):
         while True:
-            for i in self.main_list:
+            for i in self.global_server_list:
                 start = time.clock()
                 # Send QoS message
                 i[1].sendto(self.create_request_array(9999, "QoS_test"), i[0])
@@ -17,12 +20,15 @@ class Gos:
                 self.receive_data(i[0])
                 delay = time.clock() - start
                 if delay > self.THRESHOLD:
-                    self.active_list.remove(i)
+                    print("Server added to active list: " + i)
+                    self.active_server_list.remove(i)
                 else:
-                    if i not in self.active_list:
-                        self.active_list.append(i)
+                    if i not in self.active_server_list:
+                        print("Server removed from active list: " + i)
+                        self.active_server_list.append(i)
 
-    def create_request_array(self, frame_number, movie_title):
+    @staticmethod
+    def create_request_array(frame_number, movie_title):
         barr = bytearray()
         barr.extend("{}".format(frame_number))
         barr.extend(bytearray(5-len(barr)))
@@ -33,10 +39,12 @@ class Gos:
             barr.extend(bytearray(40-len(barr)))
         return barr
 
-    def receive_data(self, socket):
+    @staticmethod
+    def receive_data(socket):
         global data
         bytes_received = len(data)
         data += socket.recvfrom(1029-bytes_received)[0]
+        frame_number = 0000
         if len(data) >= 1029:
             frame_number = int(data[:5].split(b'\0', 1)[0])
             # frame_data = data[6:1029].split(b'\0', 1)[0]
@@ -47,3 +55,29 @@ class Gos:
         if frame_number == "9999":
             return True
         return False
+
+if __name__ == "__main__":
+    UDP_IP = sys.argv[1]
+    UDP_PORT = int(sys.argv[2])
+    server1 = (UDP_IP, UDP_PORT)
+
+    UDP_IP = sys.argv[3]
+    UDP_PORT = int(sys.argv[4])
+    server2 = (UDP_IP, UDP_PORT)
+
+    UDP_IP = sys.argv[5]
+    UDP_PORT = int(sys.argv[6])
+    server3 = (UDP_IP, UDP_PORT)
+
+    UDP_IP = sys.argv[7]
+    UDP_PORT = int(sys.argv[8])
+    server4 = (UDP_IP, UDP_PORT)
+
+    sock1 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock2 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock3 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock4 = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    global_server_list = [(server1, sock1), (server2, sock2), (server3, sock3), (server4, sock4)]
+    active_server_list = [(server1, sock1), (server2, sock2), (server3, sock3), (server4, sock4)]
+    qos = QoS("lock", global_server_list, active_server_list)
+    qos.start()
