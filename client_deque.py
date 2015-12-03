52295b91528a0fcca531645bba13ea1ebc913434
Tommi Brandstack <tommi.brandstack@gmail.com>
2015-12-02 19:11:38 -0600

    Qos Integration 4

--- client_deque.py
+++ client_deque.py
@@ -243,9 +243,9 @@ active_server_list = [(server1, sock1), (server2, sock2), (server3, sock3), (ser
 
 # Start Qos thread
 # task = QoS.start()
-QoS(active_list_lock, global_server_list, active_server_list)
-t = threading.Thread(target=QoS, args=(active_list_lock, global_server_list, active_server_list))
-t.start()
+#QoS(active_list_lock, global_server_list, active_server_list)
+#t = threading.Thread(target=QoS, args=(active_list_lock, global_server_list, active_server_list))
+#t.start()
 
 # start user input processing thread
 commands = collections.deque()
